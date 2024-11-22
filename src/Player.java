7775af65dc752a085a997f4900c469413271320a
ObidenkoAngelina <obidenkoangelina005@gmail.com>
2024-11-22 18:05:08 +0700

    Ввод имени игрока

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,11 @@
-public class Player {
+import java.util.Random;
+import java.util.Scanner;
+class Player {
+    private Grid grid; // Ссылка на объект Grid
+    private String name;
+
+    public Player(Grid grid, String name) {
+        this.grid = grid;
+        this.name = name;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
-
 public class main {
     public static void main(String[] args) {
-        Grid dynamicGrid = new Grid();
-        dynamicGrid.initializeGrid();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя игрока: ");
+        String playerName = scanner.nextLine();
+
+        Grid dynamicGrid = new Grid(); // Создание одного объекта Grid
+        dynamicGrid.initializeGrid(); // Инициализация сетки
         for (int row = 0; row < Grid.SIZE; row++) {
             dynamicGrid.hideNumbers(row);
         }
