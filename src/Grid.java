4bebb34b2e95613034f0fb8e0e75a60a00a9eb4a
ObidenkoAngelina <obidenkoangelina005@gmail.com>
2024-11-22 17:30:08 +0700

    Создание сетки

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
+class Grid {
+    public static final int SIZE = 9;
+    private char[][] cells = new char[SIZE][SIZE];
+    private boolean[][] visible = new boolean[SIZE][SIZE];
+    private Random random = new Random();
 
-public class Grid {
+    public void printGrid() {
+        System.out.println("+---+---+---+---+---+---+---+---+---+");
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (visible[row][col]) {
+                    System.out.print("| " + cells[row][col] + " "); // Печать видимых значений
+                } else {
+                    System.out.print("|   "); // Печать скрытых значений
+                }
+            }
+            System.out.println("|");
+            System.out.println("+---+---+---+---+---+---+---+---+---+");
+        }
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class main {
+    public static void main(String[] args) {
+        Grid dynamicGrid = new Grid();
+        dynamicGrid.printGrid();
+    }
 }
